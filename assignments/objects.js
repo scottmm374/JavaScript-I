8dca8932b2b455b3630d1c1adb675709d2fcc540
Michelle Scott <scottmm374@gmail.com>
2019-04-17 19:40:24 -0400

    Challenge 1 completed in array.js

--- assignments/objects.js
+++ assignments/objects.js
@@ -107,15 +107,7 @@ console.log(intern5.multiplyNumbers(3, 4));
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-const parent = {
-  "name": "Susan",
-  "age": 70,
-  const child: { 
-   " name": "george",
-    "age": 50,
 
-  }
-};
 
 // Log the parent object's name
 
